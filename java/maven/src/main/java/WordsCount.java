29a5ae19817b1668e84340847f7ceb607cb66689
yangjungan <june.key.young@gmail.com>
2017-03-17 00:40:53 +0800

    Finish Testcase 1

--- java/maven/src/main/java/WordsCount.java
+++ java/maven/src/main/java/WordsCount.java
@@ -18,6 +18,41 @@ public class WordsCount {
 
 
     public String constructionData(String input) {
-        return "xxx 1";
+        Map hashmap = new HashMap();
+
+        // 1st split contentStr for removing single-whitespace
+        String[] strList = input.split(" ");
+        for (String item : strList) {
+            String strTrimed = item.trim();  // remove all whitespace for each split word
+            if (hashmap.containsKey(strTrimed)) {
+                Integer wordAppearCount = (Integer) hashmap.get(strTrimed);
+                hashmap.remove(strTrimed);
+                hashmap.put(strTrimed, wordAppearCount+1);
+            }
+            else {
+                hashmap.put(strTrimed, 1);  // construct a key-value object
+            }
+        }
+
+        //  sorting
+        List<Map.Entry<String, Integer>> itemList = new ArrayList<Map.Entry<String, Integer>>(hashmap.entrySet());
+        Collections.sort(itemList, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> objA, Map.Entry<String, Integer> objB) {
+                if (objB.getValue() == objA.getValue()) {
+                    return (objA.getKey()).toString().compareTo(objB.getKey());  // when value same; then key ASC
+                }
+
+                return (objB.getValue() - objA.getValue());  // value DESC
+            }
+        });
+
+        //  print object after sorting
+        String result = "";
+        for (int i = 0; i < itemList.size(); i++) {
+            String content = itemList.get(i).toString();
+            result += content.replace("=", " ") + "\n";
+        }
+
+        return result;
     }
 }
--- java/maven/src/test/java/WordsCountTest.java
+++ java/maven/src/test/java/WordsCountTest.java
@@ -9,12 +9,13 @@ public class WordsCountTest {
 
     @Test
     public void should_return_answer1st_given_eg1st() {
-        String input = "xxx";
+        String input = "When the I was small and the car I was When";
         WordsCount wc = new WordsCount();
         String result = wc.constructionData(input);
 
-        assertThat(result).isEqualTo("xxx 1");
+        assertThat(result).isEqualTo("I 2\nWhen 2\nthe 2\nwas 2\nand 1\ncar 1\nsmall 1\n");
     }
 
+    
 
 }
